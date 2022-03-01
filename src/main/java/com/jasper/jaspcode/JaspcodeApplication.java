77af240754d343a42070bef09c0e6809044c228f
Ganesh Pandey <pandey.ganesh12@gmail.com>
2022-03-01 09:09:33 +0530

    spring initialzr files
    spring initialzr files

---
+++ src/main/java/com/jasper/jaspcode/JaspcodeApplication.java
@@ -0,0 +1,13 @@
+package com.jasper.jaspcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaspcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaspcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasper/jaspcode/JaspcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.jaspcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaspcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
